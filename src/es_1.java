c7c12ee61beb42240669c5a2c989964c2fdf7d15
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-02-21 00:03:02 +0100

    total es: 1

---
+++ src/es_1.java
@@ -0,0 +1,10 @@
+public class es_1 {
+    public static void main(String[] args) {
+        int base=4;
+        int height=5;
+
+        int area=base*height/2;
+
+        System.out.println(area);
+    }
+}
