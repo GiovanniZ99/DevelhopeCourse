f5d79fcf279d56a655cb902f2b4c728d8e8f738d
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-02-21 12:10:31 +0100

    total es=2

---
+++ src/es_2.java
@@ -0,0 +1,13 @@
+// Dati due caratteri, restituire la somma dei loro valori ASCII
+
+public class es_2 {
+    public static void main(String[] args) {
+    char a='a';
+
+    char b='b';
+
+    int result=(int)a+(int)b;
+        System.out.println(result);
+
+    }
+}
