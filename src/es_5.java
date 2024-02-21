111d15f129193bc3585ab73bc1c6f2ed0f36b36f
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-02-21 16:22:17 +0100

    esercizio di comparazione

---
+++ src/es_5.java
@@ -0,0 +1,20 @@
+/*Scrivi un programma che contiene un metodo che confronti due numeri interi
+e determini se sono diversi.
+Il programma dovrà stampare a video i due valori e il risultato dell'eguaglianza.*/
+public class es_5 {
+    public static void main(String[] args) {
+        int numb_1=5;
+        int numb_2=6;
+        System.out.println(comp(numb_1,numb_2));
+
+    }
+    static boolean comp(int a, int b){
+        System.out.println("il primo numero è " + a);
+        System.out.println("il secondo numero è " + b);
+        System.out.println("il risultato è ");
+        return a!=b;
+
+
+
+    }
+}
