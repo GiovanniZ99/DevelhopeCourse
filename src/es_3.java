bdee31837e4024722ea09e85b22c7fa8a8c3abad
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-02-21 13:01:28 +0100

    funzione che restituisce lunghezza di una stringa

---
+++ src/es_3.java
@@ -0,0 +1,13 @@
+//Definire una funzione che accetti in ingresso una stringa e ne restituisca la lunghezza
+public class es_3 {
+    public static void main(String[] args) {
+        String hello = "hello";
+        System.out.println("la lunghezza della stringa è: " + length(hello));
+
+
+    }
+
+    public static int length(String a) {
+        return a.length();
+    }
+}
