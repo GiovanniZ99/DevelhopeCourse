cae4f03da5f2cb9da11620f152079b01d7929168
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-02-21 14:07:59 +0100

    incremento e prodotto di due variabili

---
+++ src/es_4.java
@@ -0,0 +1,12 @@
+public class es_4 {
+    public static void main(String[] args) {
+        int number_1=5;
+        int number_2=7;
+        System.out.println(numbers(number_1, number_2));
+    }
+    static int numbers(int x, int y) {
+        x += 1;
+        y += 1;
+        return x*y;
+    }
+}
