7275f6010e020ce11c9c88c9483e5991a7750db6
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-02-23 11:51:44 +0100

    break  e continue facoltativo

---
+++ src/breakContinueFac.java
@@ -0,0 +1,21 @@
+public class breakContinueFac {
+    public static void main(String[] args) {
+        int a = 7;
+        numbers(a);
+    }
+
+    public static void numbers(int x) {
+        for (int i = 0; i <= x; i++) {
+
+            if (i == 5) {
+                continue;
+            }else{
+
+                System.out.println(i);
+
+            }
+
+        }
+    }
+}
+
