aa6cbddba760308ed1dd995e2839a7ba53a324ea
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-02-23 21:06:27 +0100

    Somma la riga di una matrice

--- src/matrix.java
+++ src/matrix.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class matrix {
+/*Scrivere un programma che contenga un metodo che permette di inizializzare
+una matrice riempita con dei valori a vostro piacimento e
+restituisca la somma degli elementi sulla prima riga*/
+public class matrix {
+    public static void main(String[] args) {
+        int[][] biMatrix = {{1, 2, 3},
+                     {4, 5, 6}
+                     };
+        System.out.println(sumMatrix(biMatrix));;
+    }
+
+    public static int sumMatrix(int[][] x) {
+        int sum=0;
+        for (int i=0;i<x[0].length;i++){
+
+            sum+=x[0][i];
+        }
+    return sum;
+    }
 }
