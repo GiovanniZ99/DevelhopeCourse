d856a5971c37b1dd4406980b0431a807fcf4f0a5
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-02-24 11:27:32 +0100

    correzione arrayFac2

--- src/arrayFac2.java
+++ src/arrayFac2.java
@@ -3,10 +3,11 @@ gli elementi duplicati in un array di interi con il valore -1.
 L'array ottenuto dovrà essere stampato a video.*/
 public class arrayFac2 {
     public static void main(String[] args) {
+
         int[] array = {2, 5, 7, 2, 9, 5, 7, 3, 2};
     changeArray(array);
-        for (int j : array) {
-            System.out.println(j);
+        for (int j=0; j<array.length;j++) {
+            System.out.println(array[j]);
         }
 
     }
@@ -15,9 +16,11 @@ public class arrayFac2 {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] == array[j]) {
+                    array[i]=-1;
                     array[j] = -1;
                 }
             }
         }
+
     }
 }
