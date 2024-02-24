47eefc0987be2db0f88e803ee847bfa8672301d3
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-02-24 13:56:44 +0100

    Correzione arrayFac2

--- src/arrayFac2.java
+++ src/arrayFac2.java
@@ -16,6 +16,7 @@ public class arrayFac2 {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] == array[j]) {
+                    array[i]= -1;
                     array[j] = -1;
                 }
             }
