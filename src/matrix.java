78a1eff04e2f5be79d8d1b1d76b3ab2255b6e6e0
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-02-25 17:05:56 +0100

    correzione matrici1

--- src/matrix.java
+++ src/matrix.java
@@ -3,18 +3,39 @@ una matrice riempita con dei valori a vostro piacimento e
 restituisca la somma degli elementi sulla prima riga*/
 public class matrix {
     public static void main(String[] args) {
-        int[][] biMatrix = {{1, 2, 3},
-                     {4, 5, 6}
-                     };
-        System.out.println(sumMatrix(biMatrix));;
+        int[][] newMatrix = new int[3][3];
+        fillMatrix(newMatrix);
+        System.out.println(sumMatrix(newMatrix));
     }
 
-    public static int sumMatrix(int[][] x) {
-        int sum=0;
-        for (int i=0;i<x[0].length;i++){
 
-            sum+=x[0][i];
+    public static void fillMatrix(int[][] matrix) {
+
+        int valueMatrix = 2;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] += valueMatrix;
+                valueMatrix++;
+                System.out.print(matrix[i][j]+ " ");
+
+            }
+            System.out.println();
+
         }
-    return sum;
+
+
+
     }
+    public static int sumMatrix(int [][] matrixA) {
+        int sum=0;
+
+            for(int j=0;j<matrixA[0].length;j++){
+               sum+=matrixA[0][j];
+            }
+
+
+        return sum;
+    }
+
+
 }
