e0681bdd8e43387f71e2cdd2eb0282b26335de6d
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-02-26 18:30:42 +0100

    numero e giorno della settimana

--- src/switchfac.java
+++ src/switchfac.java
@@ -2,7 +2,7 @@
 /*Scrivi un programma che contenga un metodo che prende in ingresso un valore numerico e
 restituisca il nome del giorno della settimana come stringa o una stringa di errore
  nel caso di valore maggiore di 7 o minore di 1.*/
-public class switchFac {
+public class switchfac {
     public static void main(String[] args) {
 
 
