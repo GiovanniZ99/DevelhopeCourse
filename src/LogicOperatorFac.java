c4feec3bac271dc572107c176e618f35c2e661c0
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-02-26 18:24:16 +0100

    Operatore logico &

--- src/LogicOperatorFac.java
+++ src/LogicOperatorFac.java
@@ -2,7 +2,7 @@
 /*Scrivi un programma che contiene un metodo che dati 2 interi in ingresso ed un valore
 di confronto verifichi se il numero di confronto è compreso tra due valori specifici e stampi
 il risultato dell'operazione comprensivo dei due limiti (inferiore e superiore)*/
-public class logicOperatorFac {
+public class LogicOperatorFac {
     public static void main(String[] args) {
     int x=2;
     int y=7;
