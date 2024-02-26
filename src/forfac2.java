3879b52d814366b6c312b777e3a14dc2395d7f09
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-02-26 18:28:59 +0100

    valore e numeri precedenti

--- src/forfac2.java
+++ src/forfac2.java
@@ -2,7 +2,7 @@
 il massimo numero di risultati che vuoi avere e stampi tutti i numeri naturali in ordine
 inverso (quindi se passo il punto di partenza a 6 e gli dico di restituirmi
 3 risultati mi aspetto in uscita [6 5 4] stampati).*/
-public class forFac2 {
+public class forfac2 {
     public static void main(String[] args) {
         int a=7;
         int b=2;
