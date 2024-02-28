53e2e8e42e5575789d2ce7546f6c2f2a54ebf309
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-02-28 16:47:37 +0100

    Classe Studente

---
+++ src/ObjectFac1.java
@@ -0,0 +1,12 @@
+/*Scrivere un programma che contenga una classe che definisce uno Studente che abbia
+come proprietà nome, cognome ed identificativo.
+Per questa classe saranno necessari i metodi accessori (getters e setters) ed un costruttore.
+Nel main dichiarare un oggetto di tipo Studente*/
+
+public class ObjectFac1 {
+    public static void main(String[] args) {
+    Studente studente1=new Studente("Luca","Rossi", 12345678);
+    }
+
+}
+
