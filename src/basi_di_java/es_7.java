b793e49501fa30a5709afe230ae14e38578e27d3
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-07 20:39:39 +0100

    Esercizio lunghezza delle stringhe

--- src/basi_di_java/es_7.java
+++ src/basi_di_java/es_7.java
@@ -1,3 +1,5 @@
+package basi_di_java;
+
 /*Scrivi un programma che calcoli la lunghezza di una stringa, sulla base di questa
  lunghezza stampi "Lunghezza maggiore di 10", "Lunghezza minore di 10" o "Lunghezza pari a 10".*/
 public class es_7 {
