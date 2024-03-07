6c14d6037d52dec9788055158171770375194671
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-07 20:40:32 +0100

    esercizio  break e continue

--- src/basi_di_java/es_9.java
+++ src/basi_di_java/es_9.java
@@ -1,3 +1,5 @@
+package basi_di_java;
+
 /*Scrivi un programma che contenga un metodo che prende in ingresso un valore numerico
 e stampa tutti i numeri fino al valore immesso,
 la stampa dovrà interrompersi se il valore è uguale a 5.*/
