e59dddd8d59a9fc2540a345cffa05d6c8d5229cb
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-07 20:38:29 +0100

    Metodo comparazione tra numeri

--- src/basi_di_java/es_5.java
+++ src/basi_di_java/es_5.java
@@ -1,3 +1,5 @@
+package basi_di_java;
+
 /*Scrivi un programma che contiene un metodo che confronti due numeri interi
 e determini se sono diversi.
 Il programma dovrà stampare a video i due valori e il risultato dell'eguaglianza.*/
