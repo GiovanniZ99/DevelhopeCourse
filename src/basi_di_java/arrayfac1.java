a4082e4d3cd02e94bea33251020dcd5c3bf3bcc9
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-07 20:28:32 +0100

    Esercizio riempimento array

--- src/basi_di_java/arrayfac1.java
+++ src/basi_di_java/arrayfac1.java
@@ -1,3 +1,5 @@
+package basi_di_java;
+
 /*Scrivi un programma che contenga un metodo che crea un array e lo riempie con
 i numeri da 1 a 10 e che che calcoli la somma dei numeri contenuti
 nell'array inizializzato e la stampi a video.*/
