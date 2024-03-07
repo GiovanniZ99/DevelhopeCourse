4f2093e9297810925146b6621f4d9f3c924c3b11
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-07 20:42:31 +0100

    Ordinamento col for

--- src/basi_di_java/forfac2.java
+++ src/basi_di_java/forfac2.java
@@ -1,3 +1,5 @@
+package basi_di_java;
+
 /*Scrivi un programma che contenga un metodo che prende in ingresso un numero e
 il massimo numero di risultati che vuoi avere e stampi tutti i numeri naturali in ordine
 inverso (quindi se passo il punto di partenza a 6 e gli dico di restituirmi
