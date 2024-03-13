0aeafd89440e0bf5aa8e118e6ff7b6294c0b2c79
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-13 23:06:13 +0100

    Esercizi gestione degli errori

---
+++ src/java_advanced/Null_es1.java
@@ -0,0 +1,30 @@
+/*Scrivere una funzione che accetti in input un numeratore e un denominatore
+Controlli che il numeratore e il denominatore non siano null
+Usare la funzione appena scritta in due blocchi di codice dove si cattura l'eventuale eccezione.*/
+
+package java_advanced;
+
+public class Null_es1 {
+    public static void main(String[] args) {
+        Double num = 5.567;
+        Double den = null;
+        numDen(num, den);
+        Double num2 = null;
+        Double den2 = 3.49594;
+        numDen(num2, den2);
+
+    }
+    public static void numDen (Double numeratore, Double denominatore) {
+
+        try{
+            if (numeratore == null || denominatore == null) {
+              throw new IllegalArgumentException("Numeratore e denominatore non possono essere null");
+
+            }
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+}
