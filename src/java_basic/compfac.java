98c59b0ee0b1e6d9d21c0901bc3b15942fb7fedd
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-22 14:43:56 +0100

    Comparazioni numeri interi

--- src/java_basic/compfac.java
+++ src/java_basic/compfac.java
@@ -1,14 +1,18 @@
 package java_basic;
 
+/* Scrivi un programma che contiene un metodo che che confronti due numeri interi
+e determini se sono diversi.Il programma dovrà stampare a video i due valori e il
+risultato dell'eguaglianza. */
 public class compfac {
     public static void main(String[] args) {
-    char a='a';
-    char b='b';
+        char a = 'a';
+        char b = 'b';
         System.out.println(a);
         System.out.println(b);
-        System.out.println(comp(a,b));
+        System.out.println(comp(a, b));
     }
-    public static boolean comp(char a,char b){
-        return a==b;
+
+    public static boolean comp(char a, char b) {
+        return a == b;
     }
 }
