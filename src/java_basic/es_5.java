5ac834f0e7f846f57aec4cadafff6c376062d7ef
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-22 15:17:25 +0100

    Comparazione e stampa numeri interi

--- src/java_basic/es_5.java
+++ src/java_basic/es_5.java
@@ -1,14 +1,13 @@
 package java_basic;
 
-/*Scrivi un programma che contiene un metodo che confronti due numeri interi
+/* Scrivi un programma che contiene un metodo che confronti due numeri interi
 e determini se sono diversi.
-Il programma dovrà stampare a video i due valori e il risultato dell'eguaglianza.*/
+Il programma dovrà stampare a video i due valori e il risultato dell'eguaglianza. */
 public class es_5 {
     public static void main(String[] args) {
         int numb_1=5;
         int numb_2=6;
         System.out.println(comp(numb_1,numb_2));
-
     }
     static boolean comp(int a, int b){
         System.out.println("il primo numero è " + a);
