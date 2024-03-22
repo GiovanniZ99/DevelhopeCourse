095ec4220aa5b228757d641c1a3cc1853000b8e3
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-22 15:05:36 +0100

    Stampa carattere in unicode

--- src/java_basic/String_builder_2.java
+++ src/java_basic/String_builder_2.java
@@ -1,21 +1,14 @@
 package java_basic;
-
-
-import static jdk.internal.icu.text.UTF16.charAt;
-/*Scrivi un programma Java che abbia un metodo che prenda il carattere in Unicode in un indice
+/* Scrivi un programma Java che abbia un metodo che prenda il carattere in Unicode in un indice
 specifico all'interno di una stringa e lo stampi.
 Il metodo dovrà avere nel costruttore una stringa ed una posizione e dovrà resituire il carattere in
 unicode o una stringa di errore.
-Per la creazione della stringa di risultato dovrebbe essere utilizzato StringBuilder.
-*/
+Per la creazione della stringa di risultato dovrebbe essere utilizzato StringBuilder. */
 public class String_builder_2 {
     public static void main(String[] args) {
-
-
         String stringa = "stringa";
         int posizione = 3;
         printUnicode(stringa, posizione);
-
     }
 
     public static void printUnicode(String stringa, int posizione) {
@@ -24,10 +17,7 @@ public class String_builder_2 {
             result.append("non valida");
         }else {
             result.append(stringa.charAt(posizione));
-
         }
         System.out.println(result);
     }
-
-
 }
