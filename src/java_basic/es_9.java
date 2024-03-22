7c7924c0180d5bccb54213ba2a7ee3045c25034b
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-22 15:24:18 +0100

    Stampa numeri fino al valore di input

--- src/java_basic/es_9.java
+++ src/java_basic/es_9.java
@@ -1,16 +1,15 @@
 package java_basic;
-
-/*Scrivi un programma che contenga un metodo che prende in ingresso un valore numerico
+/* Scrivi un programma che contenga un metodo che prende in ingresso un valore numerico
 e stampa tutti i numeri fino al valore immesso,
-la stampa dovrà interrompersi se il valore è uguale a 5.*/
+la stampa dovrà interrompersi se il valore è uguale a 5. */
 public class es_9 {
     public static void main(String[] args) {
         int a = 5;
         numbers(a);
     }
 
-    public static void numbers(int x) {
-        for (int i = 0; i <= x; i++) {
+    public static void numbers(int n) {
+        for (int i = 0; i <= n; i++) {
             System.out.println(i);
             if (i == 5) {
                 break;
