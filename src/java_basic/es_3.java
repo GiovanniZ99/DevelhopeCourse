0539ace02dfb6567cc22ce4098f28f4e15961df5
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-22 15:12:03 +0100

    Calcolo lunghezza di una stringa

--- src/java_basic/es_3.java
+++ src/java_basic/es_3.java
@@ -1,6 +1,6 @@
 package java_basic;
 
-//Definire una funzione che accetti in ingresso una stringa e ne restituisca la lunghezza
+// Definire una funzione che accetti in ingresso una stringa e ne restituisca la lunghezza
 public class es_3 {
     public static void main(String[] args) {
         String hello = "hello";
