8023ee15d3569a9a3d66343fbb4d568e6bcd2dcc
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-22 14:59:45 +0100

    Comparazione valori compresi

--- src/java_basic/LogicOperatorFac.java
+++ src/java_basic/LogicOperatorFac.java
@@ -1,8 +1,8 @@
 package java_basic;
 
-/*Scrivi un programma che contiene un metodo che dati 2 interi in ingresso ed un valore
+/* Scrivi un programma che contiene un metodo che dati 2 interi in ingresso ed un valore
 di confronto verifichi se il numero di confronto è compreso tra due valori specifici e stampi
-il risultato dell'operazione comprensivo dei due limiti (inferiore e superiore)*/
+il risultato dell'operazione comprensivo dei due limiti (inferiore e superiore) */
 public class LogicOperatorFac {
     public static void main(String[] args) {
     int x=2;
