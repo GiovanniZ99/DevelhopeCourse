6afd513f506d5206bdcae92e33d0a0b389bacf89
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-22 15:19:37 +0100

    Calcolo lunghezza di una stringa

--- src/java_basic/es_7.java
+++ src/java_basic/es_7.java
@@ -1,7 +1,7 @@
 package java_basic;
 
-/*Scrivi un programma che calcoli la lunghezza di una stringa, sulla base di questa
- lunghezza stampi "Lunghezza maggiore di 10", "Lunghezza minore di 10" o "Lunghezza pari a 10".*/
+/* Scrivi un programma che calcoli la lunghezza di una stringa, sulla base di questa
+ lunghezza stampi "Lunghezza maggiore di 10", "Lunghezza minore di 10" o "Lunghezza pari a 10". */
 public class es_7 {
     public static void main(String[] args) {
         String stringa="Stringa";
