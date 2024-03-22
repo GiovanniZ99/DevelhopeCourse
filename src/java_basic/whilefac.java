17032b08e1c4b3e8b77d2cd374158510dcee04b5
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-22 15:08:37 +0100

    Somma di valori precedenti al valore di input

--- src/java_basic/whilefac.java
+++ src/java_basic/whilefac.java
@@ -1,9 +1,9 @@
 package java_basic;
 
-/*Scrivere un programma che contenga un metodo che sfrutti il while loop e prenda in
+/* Scrivere un programma che contenga un metodo che sfrutti il while loop e prenda in
 ingresso 1 valore intero come limite superiore e calcoli la somma di tutti i valori prima
 del limite e la stampi a video.
-(Esempio passo 5 come limite e otterrò la somma di 1 + 2 + 3 + 4)*/
+(Esempio passo 5 come limite e otterrò la somma di 1 + 2 + 3 + 4) */
 public class whilefac {
     public static void main(String[] args) {
 
