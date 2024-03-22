438a35325cca9b1ce51f197936f8117bd6ac8cc8
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-22 15:15:32 +0100

    Funzione che incrementa e moltiplica

--- src/java_basic/es_4.java
+++ src/java_basic/es_4.java
@@ -1,5 +1,7 @@
 package java_basic;
-
+/* Scrivi un programma che contiene un metodo che date due variabili le incrementi di un valore scelto
+da te e le moltiplichi fra di loro. Il metodo dovrà restituire il risultato dell'operazione che dovrà
+essere stampato a video. */
 public class es_4 {
     public static void main(String[] args) {
         int number_1=5;
