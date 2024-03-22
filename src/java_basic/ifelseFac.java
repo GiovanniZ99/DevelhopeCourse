84ce26358652a66b962e89eae874ec83f5758924
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-22 14:55:00 +0100

    Stampa numeri e stringhe per multipli di 3 e 5

--- src/java_basic/ifelseFac.java
+++ src/java_basic/ifelseFac.java
@@ -1,5 +1,7 @@
 package java_basic;
-
+/*Scrivere un programma che stampi i numeri da 1 a 100. Per i multipli di 3 stampi
+"Fizz" al posto del numero e per i multipli di 5 stampi "Buzz".
+Nel caso in cui un numero sia multiplo di entrambi stampi "FizzBuzz".*/
 public class ifelseFac {
     public static void main(String[] args) {
         String tre = "freeze";
