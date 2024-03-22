cf27033d17524f282d473603daaa89dd7358cc47
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-22 15:01:28 +0100

    Riempimento matrice e scambio righe e colonne

--- src/java_basic/matrix2.java
+++ src/java_basic/matrix2.java
@@ -1,4 +1,5 @@
-package java_basic;/*Scrivere un programma che contenga un metodo che permette di inizializzare
+package java_basic;
+/* Scrivere un programma che contenga un metodo che permette di inizializzare
 una matrice e di scambiare le sue righe e le sue colonne stampandola a video.
 Matrice di partenza
 1 2 3
