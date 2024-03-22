db899d512e6d110f4353a221642dabf2aad244bb
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-22 14:51:28 +0100

    Stampa numeri naturali inversi da un input

--- src/java_basic/forfac2.java
+++ src/java_basic/forfac2.java
@@ -1,9 +1,9 @@
 package java_basic;
 
-/*Scrivi un programma che contenga un metodo che prende in ingresso un numero e
+/* Scrivi un programma che contenga un metodo che prende in ingresso un numero e
 il massimo numero di risultati che vuoi avere e stampi tutti i numeri naturali in ordine
 inverso (quindi se passo il punto di partenza a 6 e gli dico di restituirmi
-3 risultati mi aspetto in uscita [6 5 4] stampati).*/
+3 risultati mi aspetto in uscita [6 5 4] stampati). */
 public class forfac2 {
     public static void main(String[] args) {
         int a=7;
