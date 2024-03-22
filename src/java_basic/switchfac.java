b107cfb180a853242dbab2ee9f456ac034ebd3ef
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-22 15:07:45 +0100

    Stampa giorno settimana da un input di int

--- src/java_basic/switchfac.java
+++ src/java_basic/switchfac.java
@@ -1,15 +1,13 @@
 package java_basic;
-
-/*Scrivi un programma che contenga un metodo che prende in ingresso un valore numerico e
+/* Scrivi un programma che contenga un metodo che prende in ingresso un valore numerico e
 restituisca il nome del giorno della settimana come stringa o una stringa di errore
- nel caso di valore maggiore di 7 o minore di 1.*/
+ nel caso di valore maggiore di 7 o minore di 1. */
 public class switchfac {
     public static void main(String[] args) {
 
-
-
-
-            }
+        System.out.println(week(3));
+        System.out.println(week(8));
+    }
     public static String week(int x){
         String result = "";
         switch (x) {
