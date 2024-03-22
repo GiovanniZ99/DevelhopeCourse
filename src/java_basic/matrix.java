a8ee7b203a24433ab064cff7e264be8f450a48c1
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-03-22 15:00:51 +0100

    Riempimento matrice e somma prima riga

--- src/java_basic/matrix.java
+++ src/java_basic/matrix.java
@@ -1,8 +1,8 @@
 package java_basic;
 
-/*Scrivere un programma che contenga un metodo che permette di inizializzare
+/* Scrivere un programma che contenga un metodo che permette di inizializzare
 una matrice riempita con dei valori a vostro piacimento e
-restituisca la somma degli elementi sulla prima riga*/
+restituisca la somma degli elementi sulla prima riga */
 public class matrix {
     public static void main(String[] args) {
         int[][] newMatrix = new int[3][3];
