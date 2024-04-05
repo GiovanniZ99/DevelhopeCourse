aa7e2e16c2db2328bdad7bc692e6846aab5cb78d
Giovanni Zaffinelli <giovanni.zaffinelli99@gmail.com>
2024-04-06 00:34:38 +0200

    Classe con builder pattern in input

---
+++ src/builder/Main.java
@@ -0,0 +1,19 @@
+package builder;
+/* Crea una classe Person con i campi firstName, lastName, age e address, i getter and i setter
+Il costruttore accetterà un Builder in input
+Creare una classe Builder che attraverso il costruttore e dei metodi specifici
+(tranne nome e cognome, gli altri campi sono opzionali) per costruire l'oggetto Person.
+Creare due oggetti Person e stamparli a video */
+public class Main {
+    public static void main(String[] args) {
+        Person person = new PersonBuilder("Ciro", "Esposito")
+                .setAge(25)
+                .setAddress("Via Napoli")
+                .build();
+        Person person2 = new PersonBuilder("Nunzio", "Esposito")
+                .setAge(7)
+                .setAddress("Via Pizza")
+                .build();
+        System.out.printf("%s, %s", person, person2);
+    }
+}
---
+++ src/builder/PersonBuilder.java
@@ -0,0 +1,33 @@
+package builder;
+public class PersonBuilder {
+    private final String firstName;
+    private final String lastName;
+    private int age;
+    private String address;
+
+
+    public PersonBuilder(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public PersonBuilder setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    public Person build() {
+        return new Person(this);
+    }
+}
